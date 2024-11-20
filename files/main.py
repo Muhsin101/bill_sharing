2a71fd647b386001eece014d2766bea0f3d2c155
Muhsin Rahman <muhsin.rahman06@gmail.com>
2024-11-20 20:15:25 +0000

    created a pay method

--- files/main.py
+++ files/main.py
@@ -1,3 +1,9 @@
 from bill import *
+from flatmate import *
+from pdf_report import *
 
-bill = Bill("Electricity", 200, "3 months")
+bill = Bill(name="Electricity", amount=120, period= "March 2021")
+john = Flatmate("John", 20)
+mary = Flatmate("Mary", 25)
+print(f"John pays: £{john.pays(bill=bill, flatmate2=mary)}")
+print(f"Mary pays: £{mary.pays(bill=bill, flatmate2=john)}")
---
+++ files/pdf_report.py
@@ -0,0 +1,13 @@
+from fileinput import filename
+
+
+class PDFReport:
+    """
+    Creates a PDF file that contains data about the flatmates
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self):
+        pass
