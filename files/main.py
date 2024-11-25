7e44f65539321a33819a7f62fdd83aaa4ce67094
Muhsin Rahman <muhsin.rahman06@gmail.com>
2024-11-25 20:12:24 +0000

    finished cli

--- files/main.py
+++ files/main.py
@@ -2,15 +2,20 @@ from bill import *
 from flatmate import *
 from pdf_report import *
 
-name = input("Enter the type of bill? ")
+bill_name = input("Enter the type of bill? ")
 amount = float(input("Enter the bill amount? "))
 period = input("What month of the year is the bill for? ")
+bill = Bill(bill_name, amount, period)
 
-bill = Bill(name, amount, period)
-john = Flatmate("John", 20)
-mary = Flatmate("Mary", 25)
-print(f"John pays: £{round(john.pays(bill=bill, flatmate2=mary), 2)}")
-print(f"Mary pays: £{round(mary.pays(bill=bill, flatmate2=john), 2)}")
+name1 = input("What is your name? ")
+days_in_house1 = input(f"How many days did you: {name1} stay in house? ")
+flatmate1 = Flatmate(name1, days_in_house1)
+
+name2 = input("What is your flatmates name? ")
+days_in_house2 = input("How many days did your flatmate stay in house? ")
+flatmate2 = Flatmate(name2, days_in_house2)
+print(f"John pays: £{round(flatmate1.pays(bill, flatmate2), 2)}")
+print(f"Mary pays: £{round(flatmate2.pays(bill, flatmate1), 2)}")
 
 pdf_report = PDFReport(filename="Report1.pdf")
-pdf_report.generate(flatmate1=john, flatmate2=mary, bill=bill)
+pdf_report.generate(flatmate1, flatmate2, bill)
