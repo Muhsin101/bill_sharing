62da04d7ddf0037bf8833c609182ed420fd67405
Muhsin Rahman <muhsin.rahman06@gmail.com>
2024-11-25 20:22:20 +0000

    made some changes

--- files/main.py
+++ files/main.py
@@ -8,14 +8,14 @@ period = input("What month of the year is the bill for? ")
 bill = Bill(bill_name, amount, period)
 
 name1 = input("What is your name? ")
-days_in_house1 = input(f"How many days did you: {name1} stay in house? ")
+days_in_house1 = float(input(f"How many days did you: {name1} stay in house? "))
 flatmate1 = Flatmate(name1, days_in_house1)
 
 name2 = input("What is your flatmates name? ")
-days_in_house2 = input("How many days did your flatmate stay in house? ")
+days_in_house2 = float(input("How many days did your flatmate stay in house? "))
 flatmate2 = Flatmate(name2, days_in_house2)
-print(f"John pays: £{round(flatmate1.pays(bill, flatmate2), 2)}")
-print(f"Mary pays: £{round(flatmate2.pays(bill, flatmate1), 2)}")
+print(f"{name1} pays: £{round(flatmate1.pays(bill, flatmate2), 2)}")
+print(f"{name2} pays: £{round(flatmate2.pays(bill, flatmate1), 2)}")
 
 pdf_report = PDFReport(filename="Report1.pdf")
 pdf_report.generate(flatmate1, flatmate2, bill)
